476537cb60dd39916bec30998ee5d0109bb14f93
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-25 22:22:12 +0300

    Added stage 2

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,11 +1,17 @@
 package bot;
-import static java.lang.System.out;
 
-import static java.lang.System.out;
+import java.util.Scanner;
 
 public class SimpleBot {
     public static void main(String[] args) {
-        out.println("Hello! My name is Aid.");
-        out.println("I was created in 2020.");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+
+        System.out.println("What a great name you have, {yourName}!");
     }
 }
