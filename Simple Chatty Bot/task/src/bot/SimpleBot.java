cdbaed827153fd04d87085f70f46ef893086ddf7
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-27 20:00:22 +0300

    Added solution

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -16,12 +16,20 @@ public class SimpleBot {
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        // reading all remainders
-        final int remainder3 = scanner.nextInt();
-        final int remainder5 = scanner.nextInt();
-        final int remainder7 = scanner.nextInt();
-        final int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
 
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        // read a number and count to it here
+        int number = scanner.nextInt();
+        for (int i = 0; i < number; ++i) {
+            System.out.println(i + "!");
+        }
+        System.out.println("Completed, have a nice day!");
     }
 }
