1ce7dff350aa91f994fc25676af217daf31b582d
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-27 19:52:59 +0300

    Solved  The height of a tree (v1)

--- Problems/The height of a tree (v1)/src/Main.java
+++ Problems/The height of a tree (v1)/src/Main.java
@@ -4,18 +4,13 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        // put your code here
         final var scanner = new Scanner(System.in);
         final var nodesNumber = scanner.nextInt();
         final var nodes = scanner.tokens().mapToInt(Integer::parseInt).toArray();
 
-        IntUnaryOperator getDeep = (int index) -> {
-            int deep = 1;
-            for (int i = index; nodes[i] != -1; i = nodes[i]) {
-                ++deep;
-            }
-            return deep;
-        };
+        final IntUnaryOperator getDeep = index ->
+                (int) IntStream.iterate(index, i -> nodes[i] > -1, i -> nodes[i]).count() + 1;
+
         final var maxDeep = IntStream.range(0, nodesNumber).map(getDeep).max().orElse(0);
 
         System.out.println(maxDeep);
